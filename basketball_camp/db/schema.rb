18b05e99e9131d7c17d3255a574ec7ada649dd59
DadeCountyCoder <dadecountycoder@gmail.com>
2020-10-27 04:06:15 -0400

    redo-clear/ schema

--- basketball_camp/db/schema.rb
+++ basketball_camp/db/schema.rb
@@ -11,12 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201025225111) do
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "password"
-    t.string "email"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
